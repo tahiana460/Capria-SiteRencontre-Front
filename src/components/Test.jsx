fa762230bdd99abec66247736d1cfa3c2baffbd9
Tahiana <tahianarazakanome@gmail.com>
2023-07-21 17:31:55 +0300

    template integration

--- src/components/Test.jsx
+++
@@ -1,7 +0,0 @@
-export default function Test() {
-    return (
-        <>
-            <button className="btn btn-danger" onClick={() => {alert('oay lelena');}}>Test</button>
-        </>
-    );
-}
