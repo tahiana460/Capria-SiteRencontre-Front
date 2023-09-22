e51ab7eac33b0ed9f0099e3d02b792d32a3f3595
Tahiana <tahianarazakanome@gmail.com>
2023-09-22 11:47:52 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  // middleware: false,
+  // middleware: true,
+  output:'hybrid',
   experimental:{
     redirects:true
   },
