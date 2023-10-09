06499598a8040ee6778f12a38690f980c9de8eed
Tahiana <tahianarazakanome@gmail.com>
2023-10-09 12:31:00 +0300

    registration fix

--- src/components/Registration_step1.jsx
+++ src/components/Registration_step1.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 import api from "../const/api"
 import './Registration.css'
 
--- src/components/Registration_step2.jsx
+++ src/components/Registration_step2.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 
 export class Registation_step2 extends Component {
 
